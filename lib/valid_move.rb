c585a69ce06ee0e33f111e359446446f18dbde9f
mikedorsey1021@gmail.com dorsey <mikedorsey1021@gmail.com>
2018-04-21 04:13:59 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,7 +3,7 @@ def valid_move (board, index)
   if index.between? && position_taken?(board, index)
     true
   end
-end 
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken? (board, index)
